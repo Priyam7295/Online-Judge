3c152c8b7e739d7079714e198e9d1c5470d3b11a
Priyam169 <120120046+Priyam169@users.noreply.github.com>
2024-05-29 02:13:25 +0530

    authentication completed

--- frontend/src/Specify.jsx
+++ frontend/src/Specify.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
-
 import image_one from './assets/girlcoder.png'
 import Image from './assets/smartBoy.png'
 import './specify.css'
 
 function Specify() {
   return (
-    <div>
-
+    <div class="specifyFeatures">
         <div className="specify1" >
             <div className="specify-item">
                 <div className="specify-item-child" >
@@ -16,7 +14,7 @@ function Specify() {
                 </div  >
                 <div className="specify-item-child practice " >
                     <h1>Feeling bored with just learning DSA concepts?</h1>
-                    <p>"Strengthen your grasp on DSA topics by diving into practical questions and challenges on our code practice website. Engage in hands-on problem-solving to master Data Structures and Algorithms effectively."</p>
+                    <p>"Strengthen your grasp on DSA topics by diving into practical questions and challenges on our code practice website. Engage in hands-on problem-solving to master Data Structures and Algorithms effectively. We believe in Learning by doing."</p>
                 </div>
             </div>
         </div>
